38351a5438536a2a5da514c1accdf1d7ed99291d
Yudi Prayoga <48299631+metagenes@users.noreply.github.com>
2023-11-14 13:49:23 +0000

    feat: add auth error response

--- app-laravel/app/Exceptions/Handler.php
+++ app-laravel/app/Exceptions/Handler.php
@@ -4,7 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Str;
 class Handler extends ExceptionHandler
 {
     /**
@@ -26,5 +27,27 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+     /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => 'Unauthenticated',
+            'status' => 401,
+            'ref_id' => Str::uuid(),
+        ];
+
+        if ($request->expectsJson()) {
+            return response()->json($data, 401);
+        }
+
+        return response()->json($data, 401);
     }
 }
